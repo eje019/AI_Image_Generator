a4f6c3421f98b5c008092b69ebf9fbd51e072477
eje019 <emricndayake@gmail.com>
2025-12-13 15:35:59 +0000

    thème au chargement de la page en fonction de la preference ou dus systeme

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const themeToggle = document.querySelector('.theme-toggle');
 
+//appliquer le thème au chargement de la page en fonction de la preference ou dus systeme
+(() => {
+    //vérifier le thème enregistré dans le localStorage
+    const savedTheme = localStorage.getItem('theme');
+    const systemPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches; 
+    
+    const isDarkTheme = savedTheme === 'dark' || (!savedTheme && systemPrefersDark);
+    document.body.classList.toggle('dark-mode', isDarkTheme);
+    themeToggle.querySelector('i').className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+})();
+
 //switcher le thème et l'icône
 const toggleTheme = () => {
     const isDarkTheme = document.body.classList.toggle('dark-mode');
