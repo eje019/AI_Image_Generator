d94f38542d601ca869d2c61a7ebb982f13d08aca
eje019 <emricndayake@gmail.com>
2025-12-13 14:48:09 +0000

    icône

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const themeToggleBtn = document.querySelector('.theme-toggle');
+const themeToggle = document.querySelector('.theme-toggle');
 
 //switcher le thème et l'icône
 const toggleTheme = () => {
     const isDarkTheme = document.body.classList.toggle('dark-mode');
-    themeToggleBtn.querySelector('i').className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+    localStorage.setItem('theme', isDarkTheme ? 'dark' : 'light');
+    themeToggle.querySelector('i').className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
 }
 
-
-themeToggleBtn.addEventListener('click', () => {
-
-});
+themeToggle.addEventListener('click', toggleTheme);
