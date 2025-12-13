4c916197c1482d1a1403be59568d39b8a4964407
eje019 <emricndayake@gmail.com>
2025-12-13 14:38:26 +0000

    switcher le thème et licône

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const themeToggleBtn = document.querySelector('.theme-toggle');
 
+//switcher le thème et l'icône
+const toggleTheme = () => {
+    const isDarkTheme = document.body.classList.toggle('dark-mode');
+    themeToggleBtn.querySelector('i').className = isDarkTheme ? "fa-solid fa-sun" : "fa-solid fa-moon";
+}
+
+
 themeToggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+
 });
